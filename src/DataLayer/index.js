7067335cb1c5edf629d6a2e04585f0e20148e12d
mariamkhoudier <65205032+mariamkhoudier@users.noreply.github.com>
2020-08-03 13:23:30 +1000

    first commit

---
+++ src/DataLayer/index.js
@@ -0,0 +1,10 @@
+
+
+const StudentDataReader = require("./StudentDataReader");
+const TeacherDataReader = require("./TeacherDataReader");
+
+module.exports = {
+    StudentDataReader,
+    TeacherDataReader
+
+}
